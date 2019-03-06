4577b4b336290a9caf17ed53fed86e36cab1839d
“Brenda” <odonnell.brenda@gmail.com>
2019-03-06 14:52:28 +0000

    wrote test for increase temperature function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,6 +1,18 @@
 describe('Thermostat', function() {
+  var thermostat
+
+  beforeEach(function() {
+    thermostat = new Thermostat()
+  })
+
+
   it('should have a starting temperature of 20 degrees', function() {
-    var thermostat = new Thermostat()
     expect(thermostat.temperature).toEqual(20)
   })
+
+  it('should increase the temperature by 10', function() {
+    thermostat.up(10)
+    expect(thermostat.temperature).toEqual(30)
+  })
+
 })
