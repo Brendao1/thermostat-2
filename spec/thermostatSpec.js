d13d17e09e980eddb183621e58e8f9012150451e
Simon <01secn@gmail.com>
2019-03-06 16:19:43 +0000

    Added test to reset temp to 20

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -14,31 +14,36 @@ describe('Thermostat', function() {
     expect(thermostat.temp).toEqual(25)
   })
 
-  it('should decreases the temperature by 10', function() {
+  it('should decrease the temperature by 10', function() {
     thermostat.down(10)
     expect(thermostat.temp).toEqual(10)
   })
 
-  it("doesn't go below 10 degrees", function(){
+  it("doesn't go below 10 degrees", function() {
     var error = function() {
       thermostat.down(11)
     }
     expect(error).toThrow('Min temperature is 10 degrees')
   })
 
-  it("should limit to max temp of 25 degrees if power saving mode is on", function(){
-    thermostat.powerSavingMode = true
+  it("should limit to max temp of 25 if power saving mode is on (which is default)", function() {
     var increaseTempBeyondMax = function() {
       thermostat.up(6)
     }
     expect(increaseTempBeyondMax).toThrow('Max temperature is 25 degrees')
   })
 
-  it("should limit to max temp of 32 degrees if power saving mode is off", function(){
+  it("should limit to max temp of 32 degrees if power saving mode is off", function() {
     thermostat.powerSavingMode = false
     var increaseTempBeyondMax = function() {
       thermostat.up(13)
     }
     expect(increaseTempBeyondMax).toThrow('Max temperature is 32 degrees')
+  })
+
+  it("should reset temperature to 20", function() {
+    thermostat.up(5)
+    thermostat.reset()
+    expect(thermostat.temp).toEqual(20)
   })
 })
