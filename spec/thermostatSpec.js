3d4aa0cc7ef5af1bd48142d80144b0b87f3e9237
“Brenda” <odonnell.brenda@gmail.com>
2019-03-06 16:13:13 +0000

    added failing test for max temperature when power saving mode is off

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -26,11 +26,19 @@ describe('Thermostat', function() {
     expect(error).toThrow('Min temperature is 10 degrees')
   })
 
-  it("should return a max temp of 25 degrees if power saving mode is on", function(){
+  it("should limit to max temp of 25 degrees if power saving mode is on", function(){
     thermostat.powerSavingMode = true
     var increaseTempBeyondMax = function() {
       thermostat.up(6)
     }
     expect(increaseTempBeyondMax).toThrow('Max temperature is 25 degrees')
+  })
+
+  it("should limit to max temp of 32 degrees if power saving mode is off", function(){
+    thermostat.powerSavingMode = false
+    var increaseTempBeyondMax = function() {
+      thermostat.up(13)
+    }
+    expect(increaseTempBeyondMax).toThrow('Max temperature is 32 degrees')
   })
 })
